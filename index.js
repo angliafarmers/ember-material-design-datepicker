f8ae922dadb6e884ca428ea0c776f751b343e0f4
Adam Knights <adam.knights@angliafarmers.co.uk>
2016-08-05 09:21:16 +0100

    Stages 1 and 2: Setup to allow users to change SASS variables, implement validation when users are inputting via text directly

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-material-design-datepicker'
+  name: 'ember-material-design-datepicker',
+    included(app) {
+     this._super.included(app);
+   },
+   contentFor: function(type) {
+    if (type === 'head') {
+      return '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">';
+    }
+  }
 };
